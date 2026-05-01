4a669afad67240f0553ddd3eb547cbed5c595f3d
Zaks Kebab Alverca <alvercazakskebab@gmail.com>
2026-05-01 22:58:17 +0100

    save orders to firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMY-Snm1brtqNPJ7Nf8cHcO_wgR87_aDQ",
@@ -8,9 +9,10 @@ const firebaseConfig = {
   storageBucket: "zaks-kebab.firebasestorage.app",
   messagingSenderId: "281713955679",
   appId: "1:281713955679:web:9860db981a7d976c179cea",
-  measurementId: "G-8SB6NG98Z4"
+  measurementId: "G-8SB6NG98Z4",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
